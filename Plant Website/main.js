e9c76f1e6ee746d85192ad249446bd9e95c76785
Israa_Abdelhaq <israaabdellhaq@gmail.com>
2024-12-05 17:28:24 +0200

    Add Plant Website as a normal folder

---
+++ Plant Website/main.js
@@ -0,0 +1,15 @@
+const navMenu = document.getElementById("nav-menu")
+const navLinks = document.querySelectorAll('.nav-links')
+const hamburger = document.getElementById("hamburger")
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]")
+    hamburger.classList.toggle("ri-close-large-line")
+})
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle("left-[0]")
+        hamburger.classList.toggle("ri-close-large-line")
+    })
+})
